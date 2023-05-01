37a88ef04e39b594f0720e50b594d229e7a5e93a
Blase Moylan <moylanblase@gmail.com>
2023-05-01 13:54:42 -0700

    📦 NEW: added reservation form

--- frontend/src/components/Home/user_reservations.jsx
+++ frontend/src/components/Home/user_reservations.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+const UserReservations = ({username, auth}) => {
+    const [allReservations , setAllReservations]=useState([])
 
-const user_reservations = ({username, auth}) => {
-    
+    async function getReservations(){
+        let results= await axios.get(`http://127.0.0.1:5000/api/all_tables`)
+        setAllTables(results.data)
+    }
+
     return (  
         <div></div>
     );
 }
  
-export default user_reservations;
+export default UserReservations;
