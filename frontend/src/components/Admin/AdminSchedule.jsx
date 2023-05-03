cd94a4880a733ca272d3403d846feda4d28bbc13
Blase Moylan <moylanblase@gmail.com>
2023-05-02 20:38:09 -0700

    📦 NEW: schedule not complete

--- frontend/src/components/Admin/AdminSchedule.jsx
+++ frontend/src/components/Admin/AdminSchedule.jsx
@@ -9,17 +9,60 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-
-export default class DemoApp extends React.Component {
-  render() {
+import { useState } from 'react'
+import axios from 'axios'
+export default class Schedule extends React.Component {
+    render() {
+        let bookings=[]
+        async function getReservations(){
+            let results=await axios.get(`http://127.0.0.1:5000/api/all_reservations`)
+            console.log(results.data)
+            let ans=results.data
+            let booking=ans.map((item)=>{
+                let temp={title:`booked ${item.time}`,date:item.date}
+                bookings.push(temp)
+                return true
+            })
+        // for(let reservation in ans){
+        //     console.log("it is here")
+        //     console.log(reservation)
+        //     let temp={title:`booked ${reservation.time}`,date:reservation.date}
+        //     bookings.push(temp)
+        // }
+        console.log(bookings)
+      }
+      getReservations()
     return (
       <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
-        events={[
-          {title:"booked 5:00 pm",date:'2023-04-28',time:'20:00 pm'}
-        ]}
+        events={bookings}
       />
     )
   }
 }
+// export default class Schedule extends React.Component {
+//     render() {
+//       const [reservations,setReservations] = useState([])
+//       const [display,setDisplay] = useState([])
+//       async function getReservations(){
+//         let results=await axios.get(`http://127.0.0.1:5000/api/all_reservations`)
+//         setReservations(results.data)
+//       }
+//       function displayReservations(){
+//         let bookings=[]
+//         for(let reservation in reservations){
+//             let temp={title:`booked ${reservation.time}`,date:reservation.date}
+//             bookings.add(temp)
+//         }
+//         setDisplay(bookings)
+//       }
+//     return (
+//       <FullCalendar
+//         plugins={[ dayGridPlugin ]}
+//         initialView="dayGridMonth"
+//         events={display}
+//       />
+//     )
+//   }
+// }
