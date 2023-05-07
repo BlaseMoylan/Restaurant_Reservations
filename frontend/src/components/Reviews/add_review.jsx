7d0f89147e83fc2d56bfc46aea056728d0932919
Blase Moylan <moylanblase@gmail.com>
2023-05-06 19:45:46 -0700

    📦 NEW: added review functionality

--- frontend/src/components/Reviews/add_review.jsx
+++ frontend/src/components/Reviews/add_review.jsx
@@ -1,7 +1,44 @@
-const add_reviews = (props) => {
+import React, { useState, useContext } from 'react';
+import AuthContext from "../../context/AuthContext";
+import axios from 'axios';
+import FullCalendar from '@fullcalendar/react';
+const AddReviews = ({getAllReviews,allReviews}) => {
+    const { user, token } = useContext(AuthContext);
+    const [AddReview,setAddReview]=useState([])
+    const [addRating,setAddRating]=useState(0)
+    async function makeReview(form){
+    try{
+        let results= await axios.post(`http://127.0.0.1:5000/api/user_reviews`,form,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      getAllReviews()}
+        
+    catch (ex) {
+      console.log("error in put");
+    }
+    }
+    function handleSubmit(){
+        console.log(user.id)
+        let form={
+            review_text:AddReview,
+            rating:addRating,
+            // customer_id:+user.id
+        }
+        makeReview(form)
+    }
+
     return ( 
-        <div></div>
+        <form onSubmit={handleSubmit}>
+            <label>Rating</label>
+            <input type="integer" value={addRating} onChange={(event)=>setAddRating(event.target.value)} />
+            <input type="text" value={AddReview} onChange={(event)=>setAddReview(event.target.value)}/>
+            <button type='submit' className="btn btn-primary">Add Review</button>
+        </form>
     );
 }
  
-export default add_reviews;
+export default AddReviews;
